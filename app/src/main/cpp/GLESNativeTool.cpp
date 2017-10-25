242062444cea4fab11052ff0269ded0181e6a707
Eric Huang <ericczhuang@tencent.com>
2017-10-25 20:00:51 +0800

    just for demo

--- app/src/main/cpp/GLESNativeTool.cpp
+++ app/src/main/cpp/GLESNativeTool.cpp
@@ -1,13 +1,13 @@
-#include "com_android_grafika_gles_GLESNativeTool.h"
+#include "com_android_demo_GLESNativeTool.h"
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 
-JNIEXPORT void JNICALL Java_com_android_grafika_gles_GLESNativeTool_glReadPixelWithJni
+JNIEXPORT void JNICALL Java_com_android_demo_GLESNativeTool_glReadPixelWithJni
   (JNIEnv * env, jobject obj, jint x, jint y, jint width, jint height, jint format, jint type, jint offset){
 	glReadPixels(x, y, width, height, format, type, 0);
 }
 
-JNIEXPORT void JNICALL Java_com_android_grafika_gles_GLESNativeTool_convertByteBufferToByteArray
+JNIEXPORT void JNICALL Java_com_android_demo_GLESNativeTool_convertByteBufferToByteArray
   (JNIEnv * env, jobject obj, jobject input, jbyteArray output){
 
 	jbyte *c_array = env->GetByteArrayElements(output, 0);
---
+++ app/src/main/java/com/android/demo/GLESNativeTool.java
@@ -0,0 +1,10 @@
+package com.android.demo;
+
+
+
+public class GLESNativeTool {
+    static{
+        System.loadLibrary("gles_tools");
+    }
+    public static native void glReadPixelWithJni(int x, int y, int width, int height, int format, int type, int offset);
+}
